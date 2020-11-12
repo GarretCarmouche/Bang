7def7c5b6aa85b6e572d35f0bcc719a3e289262f
GarretCarmouche <garret307@yahoo.com>
2020-11-11 21:30:57 -0600

    Add AI methods and opinion calculations. Generate project javadoc

---
+++ src/bang/Player.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bang;
+
+import java.util.*;
+/**
+ *
+ * @author Garret
+ */
+
+public class Player {
+    //private Character char;
+    
+    public Player(){
+        
+    }
+}
